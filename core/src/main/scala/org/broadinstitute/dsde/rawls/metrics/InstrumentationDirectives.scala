c41b53189e2cc7e87434bbc2fb8717e42fce15ac
Joel Thibault <thibault@broadinstitute.org>
2017-07-11 17:24:06 -0400

    update RawlsInstrumented from rt-gawb-2145

--- core/src/main/scala/org/broadinstitute/dsde/rawls/metrics/InstrumentationDirectives.scala
+++ core/src/main/scala/org/broadinstitute/dsde/rawls/metrics/InstrumentationDirectives.scala
@@ -1,24 +1,20 @@
 package org.broadinstitute.dsde.rawls.metrics
 
-import nl.grons.metrics.scala.Counter
-
 import spray.routing.Directive0
-import spray.routing.directives.MiscDirectives.requestUri
+import spray.routing.directives.MiscDirectives.requestInstance
 import spray.routing.directives.BasicDirectives.pass
-import spray.http.Uri.Path
 
 trait InstrumentationDirectives extends RawlsInstrumented {
 
   override val rawlsMetricBaseName = "rawls"
 
-  def requestCounter(requestPath: Path): Counter =
-    ExpandedMetricBuilder
-      .expand(RequestPathMetric, requestPath)
-      .asCounter()
-
   // extract the request path from the URI, increment the counter, and continue processing the request
-  def instrumentRequestPath: Directive0 = requestUri flatMap { uri =>
-    requestCounter(uri.path).inc()
+
+  // TODO can I add timing here (Anu's story) - google "scala spray request timing"
+
+  def instrumentRequestPath: Directive0 = requestInstance flatMap { request =>
+    val x = ExpandedMetricBuilder.expand("temp")
+    httpRequestCounter(x)(request).inc()
     pass
   }
 }
--- core/src/main/scala/org/broadinstitute/dsde/rawls/metrics/RawlsInstrumented.scala
+++ core/src/main/scala/org/broadinstitute/dsde/rawls/metrics/RawlsInstrumented.scala
@@ -3,14 +3,18 @@ package org.broadinstitute.dsde.rawls.metrics
 import java.util.UUID
 
 import nl.grons.metrics.scala._
+import org.apache.http.HttpStatus
 import org.broadinstitute.dsde.rawls.model.SubmissionStatuses.SubmissionStatus
+import org.broadinstitute.dsde.rawls.model.Subsystems.Subsystem
 import org.broadinstitute.dsde.rawls.model.WorkflowStatuses.WorkflowStatus
-import org.broadinstitute.dsde.rawls.model.WorkspaceName
+import org.broadinstitute.dsde.rawls.model.{Subsystems, WorkspaceName}
 import slick.dbio.{DBIOAction, Effect, NoStream}
+import spray.http.Uri.Path
+import spray.http.Uri.Path.{Empty, Segment, Slash}
+import spray.http._
 
 import scala.annotation.implicitNotFound
 import scala.concurrent.ExecutionContext
-import spray.http.Uri.Path
 
 /**
   * Mixin trait for instrumentation.
@@ -23,7 +27,7 @@ trait RawlsInstrumented extends DefaultInstrumented {
   final val SubmissionMetric = "submission"
   final val SubmissionStatusMetric = "submissionStatus"
   final val WorkflowStatusMetric = "workflowStatus"
-  final val RequestPathMetric = "requestPath"
+  final val SubsystemMetric = "subsystem"
 
   /**
     * Base name for all metrics. This will be prepended to all generated metric names.
@@ -39,7 +43,8 @@ trait RawlsInstrumented extends DefaultInstrumented {
     */
   @implicitNotFound(msg = "Cannot expand instances of type ${A}")
   protected sealed trait Expansion[A] {
-    def makeName(key: String, a: A) = s"$key.${a.toString}"
+    def makeName(key: Option[String], a: A) = s"${getKey(key)}${a.toString}"
+    protected def getKey(k: Option[String]): String = k.map(k => s"$k.").getOrElse("")
   }
 
   // Typeclass instances:
@@ -47,18 +52,39 @@ trait RawlsInstrumented extends DefaultInstrumented {
   protected implicit object WorkspaceNameExpansion extends Expansion[WorkspaceName] {
     // Statsd doesn't allow slashes in metric names, so we override makeName to override
     // the default toString based implementation.
-    override def makeName(key: String, n: WorkspaceName): String = s"$key.${n.toString.replace('/', '.')}"
+    override def makeName(key: Option[String], n: WorkspaceName): String = s"${getKey(key)}${n.toString.replace('/', '.')}"
   }
 
   // Provide an implicit for UUIDs but can use the default makeName
   protected implicit object UUIDExpansion extends Expansion[UUID]
 
-  // Provide implicits for WorkflowStatus and SubmissionStatus using the default makeName.
+  // Provide implicits for various RawlsEnumerations using the default makeName.
+  // TODO: should we provide an expansion for RawlsEnumeration in general?
   //
   // These both take an upper type bound A <: WorkflowStatus|SubmissionStatus so they can
   // work either with the supertype (e.g. WorkflowStatus) or a subtype (e.g. WorkflowStatuses.Launching).
   protected implicit def WorkflowStatusExpansion[A <: WorkflowStatus] = new Expansion[A] {}
   protected implicit def SubmissionStatusExpansion[A <: SubmissionStatus] = new Expansion[A] {}
+  protected implicit def SubsystemExpansion[A <: Subsystem] = new Expansion[A] {}
+
+  protected implicit object HttpMethodExpansion extends Expansion[HttpMethod] {
+    override def makeName(key: Option[String], m: HttpMethod): String = s"${getKey(key)}${m.toString.toLowerCase}"
+  }
+
+  protected implicit object UriExpansion extends Expansion[Uri] {
+    override def makeName(key: Option[String], uri: Uri): String = s"${getKey(key)}${uri2String(uri)}"
+
+    private def uri2String(uri: Uri): String = {
+      val path = if (uri.path.startsWithSlash) uri.path.tail.toString else uri.path
+      path.toString.replace('/', '_')
+    }
+  }
+
+  protected implicit object StatusCodeExpansion extends Expansion[StatusCode] {
+    override def makeName(key: Option[String], statusCode: StatusCode): String = s"${statusCode.intValue.toString}"
+  }
+
+  protected implicit object StringExpansion extends Expansion[String]
 
   /**
     * Utility for building expanded metric names in a typesafe way. Example usage:
@@ -76,14 +102,18 @@ trait RawlsInstrumented extends DefaultInstrumented {
     *
     * Note the above will only compile if there are [[Expansion]] instances for the types passed to the expand method.
     */
-  implicit object PathExpansion extends Expansion[Path] {
-    override def makeName(key: String, p: Path): String = s"$key.${p.toString.replace('/', '.')}"
-  }
-
   protected class ExpandedMetricBuilder private (m: String = "") {
-    def expand[A: Expansion](key: String, a: A) = {
+    def expand[A: Expansion](key: Option[String], a: A): ExpandedMetricBuilder = {
       new ExpandedMetricBuilder(
         (if (m == "") m else m + ".") + implicitly[Expansion[A]].makeName(key, a))
+    }
+
+    def expand[A: Expansion](key: String, a: A): ExpandedMetricBuilder = {
+      expand(Some(key), a)
+    }
+
+    def expand[A: Expansion](a: A): ExpandedMetricBuilder = {
+      expand(None, a)
     }
 
     def asCounter(name: Option[String] = None): Counter =
@@ -92,18 +122,27 @@ trait RawlsInstrumented extends DefaultInstrumented {
     def asGauge[T](name: Option[String] = None)(fn: => T): Gauge[T] =
       metrics.gauge(makeName(name))(fn)
 
-    def asTimer(name: Option[String] = None): Timer = {
+    def asTimer(name: Option[String] = None): Timer =
       metrics.timer(makeName(name))
-    }
 
     private def makeName(name: Option[String]): String = {
       m + name.map(n => s".$n").getOrElse("")
     }
+
+    override def toString: String = m
   }
 
   object ExpandedMetricBuilder {
-    def expand[A: Expansion](key: String, a: A) = {
+    def expand[A: Expansion](key: Option[String], a: A): ExpandedMetricBuilder = {
       new ExpandedMetricBuilder().expand(key, a)
+    }
+
+    def expand[A: Expansion](key: String, a: A): ExpandedMetricBuilder = {
+      expand(Some(key), a)
+    }
+
+    def expand[A: Expansion](a: A): ExpandedMetricBuilder = {
+      expand(None, a)
     }
   }
 
@@ -140,6 +179,15 @@ trait RawlsInstrumented extends DefaultInstrumented {
     status => builder
       .expand(WorkflowStatusMetric, status)
       .asCounter()
+
+  protected def httpRequestCounter(builder: ExpandedMetricBuilder): HttpRequest => Counter =
+    httpRequest => builder.expand("request").expand(httpRequest.method).expand(httpRequest.uri).asCounter()
+
+  protected def httpResponseCounter(builder: ExpandedMetricBuilder): HttpResponse => Counter =
+    httpResponse => builder.expand("response").expand(httpResponse.status).asCounter()
+
+  protected def httpRequestTimer(builder: ExpandedMetricBuilder): HttpRequest => Timer =
+    httpRequest => builder.expand("latency").expand(httpRequest.method).expand(httpRequest.uri).asTimer()
 }
 
 object RawlsInstrumented {
