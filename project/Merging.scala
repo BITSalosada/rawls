8b265f7f579a66780be5a09fa3e41193fcd493e6
Joel Thibault <thibault@broadinstitute.org>
2017-12-12 17:29:58 -0500

    assembly merge conflict -> rm version.conf

--- project/Merging.scala
+++ project/Merging.scala
@@ -11,6 +11,7 @@ object Merging {
     case "application.conf" => MergeStrategy.first
     case "logback.xml" => MergeStrategy.first
     case "cobertura.properties" => MergeStrategy.discard
+    case "version.conf" => MergeStrategy.discard
     case "overview.html" => MergeStrategy.first
     case x => oldStrategy(x)
   }
