a95aa38604c4f61ec7cd4252b30b96e161f122ac
Joel Thibault <thibault@broadinstitute.org>
2017-12-12 17:04:08 -0500

    exclude wdl's older scala logging

--- project/Dependencies.scala
+++ project/Dependencies.scala
@@ -69,6 +69,9 @@ object Dependencies {
   val ficus: ModuleID =           "com.iheart"                    %% "ficus"                % "1.4.0"
 
   val cromwellWdl: ModuleID = ("org.broadinstitute" %% "cromwell-wdl-model-draft2" % cromwellVersion
+    exclude("org.typelevel", "cats_2.11")
+    exclude("io.spray", "spray-json_2.11")
+    exclude("com.typesafe.scala-logging", "scala-logging_2.11")
     exclude("org.typelevel", "cats_2.12")
     exclude("io.spray", "spray-json_2.12")
     exclude("io.spray", "akka-parsing_2.12")
