24a8da66fea94dc0cdce8592c932608c010254b0
Hussein Elgridly <hussein@broadinstitute.org>
2019-03-12 13:10:57 -0400

    fixed missing configS

--- core/src/main/scala/org/broadinstitute/dsde/rawls/dataaccess/HttpGoogleServicesDAO.scala
+++ core/src/main/scala/org/broadinstitute/dsde/rawls/dataaccess/HttpGoogleServicesDAO.scala
@@ -743,7 +743,7 @@ class HttpGoogleServicesDAO(
 
   def projectToDM(projectName: RawlsBillingProjectName) = s"dm-${projectName.value}"
 
-  def createProject2(projectName: RawlsBillingProjectName, billingAccount: RawlsBillingAccount, dmTemplatePath: String, pubSubTopic: String, requesterPaysRole: String, ownerGroupEmail: WorkbenchEmail, computeUserGroupEmail: WorkbenchEmail, projectTemplate: ProjectTemplate): Future[Unit] = {
+  def createProject2(projectName: RawlsBillingProjectName, billingAccount: RawlsBillingAccount, dmTemplatePath: String, requesterPaysRole: String, ownerGroupEmail: WorkbenchEmail, computeUserGroupEmail: WorkbenchEmail, projectTemplate: ProjectTemplate): Future[Unit] = {
     implicit val service = GoogleInstrumentedService.DeploymentManager
     val credential = getDeploymentManagerAccountCredential
     val deploymentManager = getDeploymentManager(credential)
@@ -752,7 +752,6 @@ class HttpGoogleServicesDAO(
       "billingAccountId" -> billingAccount.accountName.value,
       "projectId" -> projectName.value,
       "parentOrganization" -> appsDomain,
-      "pubsubTopic" -> pubSubTopic,
 
       "fcBillingUser" -> billingEmail, //FIXME: should be the billing GROUP
 
